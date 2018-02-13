38533bfa9a30966ed32a8cc375a867e4c89855bf
Tab Fitts <tfitts@spryservers.net>
2018-02-13 15:18:42 -0700

    Add script to generate client config and add to server

---
+++ addpeer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+# Generate keys
+private_key=$(wg genkey)
+public_key=$(echo $private_key | wg pubkey)
+
+# Add keys to server
+wg set wg0 peer $public_key allowed-ips $client_ip/32
+
+# Generate client config
+cat > spry-wg-us-tx1.conf << _EOF
+[Interface]
+PrivateKey = $private_key
+Address = $client_ip/32
+DNS = 10.200.200.1
+
+[Peer]
+PublicKey = kumeo8QaX4DXqSjZsKRiCuQ7dpA8u6HDWD4W0drlk1c=
+Endpoint = 69.162.114.122:51820
+AllowedIPs = 0.0.0.0/0, ::/0
+_EOF
