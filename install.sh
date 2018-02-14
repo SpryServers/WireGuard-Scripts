6219a8f386e604b271be8d2f1934672e20c35b11
Tab Fitts <tfitts@spryservers.net>
2018-02-14 10:27:37 -0700

    Remove a derp

--- install.sh
+++ install.sh
@@ -10,6 +10,5 @@ chown root $install_dir/wg-provision
 chmod u=rwx,go=xr,+s $install_dir/wg-provision
 
 cp -R src/addpeer.sh $install_dir/
-cp -R src/ip.sh $install_dir/
 chown root $install_dir/*.sh
 chmod u=rwx,go=xr $install_dir/*.sh
