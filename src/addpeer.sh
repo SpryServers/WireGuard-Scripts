4cf89f19ff0c7de5c8fa0ff096fe01df4756bd52
Tab Fitts <tfitts@spryservers.net>
2018-02-14 12:23:02 -0700

    Add copyright

--- src/addpeer.sh
+++ src/addpeer.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+#   WireGuard Provisioning Scripts
+#
+#   @author Tab Fitts
+#
+#   Copyright (C) 2018 Spry Servers, LLC. https://www.spryservers.net. All rights Reserved.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2,
+#   as published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 set -e
 
 # Generate keys
